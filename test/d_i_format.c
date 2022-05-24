8da84ea65c3c3496896ac2066f7eb249e63f9473
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-05-23 18:50:28 -0700

    updated task 2 correction

--- test/d_i_format.c
+++ test/d_i_format.c
@@ -9,30 +9,35 @@
 */
 int _putint(va_list args)
 {
-int num, i, written_num = 0, neg = 0;
+int i, written_num = 0, n;
+unsigned int num;
 char *buff;
 
-num = va_arg(args, int);
+n = va_arg(args, int);
 buff = malloc(sizeof(char));
+if (buff == NULL)
+{
+return (-1);
+}
 i = 0;
-if (num == 0)
+if (n == 0)
 {
 written_num += _putchar(48);
 }
-if (num < 0)
+if (n < 0)
 {
-num *= -1;
-neg = 1;
+num = n *= -1;
+written_num += _putchar('-');
+}
+else 
+{
+num = n;
 }
 while (num > 0)
 {
 buff[i] = (num % 10) + '0';
 num /= 10;
 i++;
-}
-if (neg)
-{
-buff[i] = '-';
 }
 buff = rev_string(buff);
 for (i = 0; buff[i] != '\0'; i++)
--- test/main.c
+++ test/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
 * main - test the -printf funct.
@@ -17,6 +18,19 @@ len2 = printf(NULL,"hello");
 printf("\n%d %d",len1, len2);
 len1= _printf("negative %d %d\n", NULL, -762534);
 len2=  printf("negative %i %d\n", NULL, -762534);
-printf("\n%d %d", len1, len2);
+printf("\n%d %d\n", len1, len2);
+int n, n2;
+
+n = _printf("hello %% world (%d) hh %s\n", 5666, "going");
+n = _printf("%d\n", INT_MIN);
+n2 =  printf ("%d\n",INT_MIN);
+n = _printf("%d\n", INT_MAX);
+n2 =  printf ("%d\n",INT_MAX);
+n = _printf("%i + %i = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+n2= printf("%i + %i = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+n = _printf("iddi%diddiiddi\n", 1024);
+n2 = _printf("iddi%iiddiiddi\n", 1024);
+printf ("%d %d", n, n2);
+
 return (0);
 }
